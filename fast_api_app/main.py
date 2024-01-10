859faec03bf07c2a4748b301719a4efb02050d3c
aleksey88bsuir <aleksey88bsuir@yandex.ru>
2024-01-10 15:15:23 +0300

    если коомментарий виден, то все ок

--- fast_api_app/main.py
+++ fast_api_app/main.py
@@ -3,6 +3,7 @@ from items_views import router as items_router
 from users.views import router as users_router
 import uvicorn
 
+# здесь оставляю комментарий
 app = FastAPI()
 app.include_router(items_router)
 app.include_router(users_router)
