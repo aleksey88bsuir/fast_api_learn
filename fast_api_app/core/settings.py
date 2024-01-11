b704c20bfa1a9044457971908bb6b51c837a240a
aleksey88bsuir <aleksey88bsuir@yandex.ru>
2024-01-11 13:22:10 +0300

    Начал писать API для взаимодействия с базой данных. Написал CRUD для product

--- fast_api_app/core/settings.py
+++ fast_api_app/core/settings.py
@@ -1,8 +1,13 @@
 from pydantic_settings import BaseSettings
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).parent.parent
+print(BASE_DIR)
 
 
 class ConfigApp(BaseSettings):
-    db_url: str = "sqlite+aiosqlite:///./shop.db"
+    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/shop.sqlite3"
     db_echo: bool = True
 
 
