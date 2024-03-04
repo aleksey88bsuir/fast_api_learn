c88c90666a77c99b6284987e8a1e6d8b0b932e81
aleksey88bsuir <aleksey88bsuir@yandex.ru>
2024-03-04 17:20:39 +0300

    Создал ссылки друг на друга в таблицах posts | users

--- fast_api_app/core/models/post.py
+++ fast_api_app/core/models/post.py
@@ -1,6 +1,12 @@
+from typing import TYPE_CHECKING
+
 from .base import Base
 from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class Post(Base):
@@ -12,4 +18,4 @@ class Post(Base):
     user_id: Mapped[int] = mapped_column(
         ForeignKey("users.id")
     )
-
+    user: Mapped["User"] = relationship(back_populates="posts")
--- fast_api_app/core/models/user.py
+++ fast_api_app/core/models/user.py
@@ -1,7 +1,15 @@
+from typing import TYPE_CHECKING
+
 from .base import Base
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+if TYPE_CHECKING:
+    from .post import Post
 
 
 class User(Base):
     name: Mapped[str] = mapped_column(String(30), unique=True)
+
+    posts: Mapped[list["Post"]] = relationship(back_populates="user")
